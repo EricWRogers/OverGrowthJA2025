a690b6e8a0704171ec21fcf638fc541122d79d54
Aiden Thacker <aidenthacker86@gmail.com>
2025-06-06 18:40:27 -0500

    Getting TownHall off the ground

--- src/ECS/ScriptableEntities/Buildings/TownHall.cpp
+++ src/ECS/ScriptableEntities/Buildings/TownHall.cpp
@@ -2,9 +2,20 @@
 
 void TownHall::OnCreate()
 {
-    //Set Town Hall Max Health
-    m_healthComponent.maxHealth = 25.0f;
-    Health::ResetHealth(m_entity);
+    if (!entity.HasComponent<HealthComponent>())
+    {
+        entity.AddComponent<HealthComponent>();
+    }
+
+    HealthComponent &health = entity.GetComponent<HealthComponent>();
+
+    health.maxHealth = 25.0f;
+
+    Health::ResetHealth(entity);
+
+    m_isAlive = true;
+
+    Canis::Log("The Current Health is " + std::to_string(health.currentHealth));
 }
 
 void TownHall::OnReady()
@@ -16,11 +27,27 @@ void TownHall::OnReady()
 void TownHall::OnDestroy()
 {
     //Lose Game Screen
-    Canis::Log("You Lose");
-    entity.GetSceneManager().Load("lose_menu");
+    Canis::Log("No Dont destroy");
+    //entity.GetSceneManager().Load("lose_menu");
+}
+
+void TownHall::OnUpdate(float _dt)
+{
+    HealthComponent &health = entity.GetComponent<HealthComponent>();
+    if(health.currentHealth == 0 && m_isAlive == true)
+    {
+        Canis::Log("You Lose");
+        entity.Destroy();
+    }
+
+    if(GetInputManager().JustPressedKey(SDLK_e))
+    {
+        Canis::Log("Button Pressed");
+        Health::Annihilation(entity);
+    }
 }
 
 void TownHall::SpawnUnit()
 {
-
+    Canis::Log("Spawning Units");
 }
