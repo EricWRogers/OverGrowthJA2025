3b9964ec4c32572fae8d330d32c033b0006fe216
Aiden Thacker <aidenthacker86@gmail.com>
2025-06-06 13:43:50 -0500

    Trying to get my scene ready to test my scripts

--- src/Registers/RegisterAiden.cpp
+++ src/Registers/RegisterAiden.cpp
@@ -3,10 +3,16 @@
 #include <Canis/App.hpp>
 
 //#include "Ball.hpp"
+#include "../ECS/Components/Health.hpp"
+#include "../ECS/ScriptableEntities/Buildings/TownHall.hpp"
+#include "../ECS/ScriptableEntities/Characters/NPC.hpp"
 
 void RegisterAiden(void* _app)
 {
     Canis::App& app = *(Canis::App*)_app;
 
     //REGISTER_SCRIPTABLE_COMPONENT(app, Ball);
+    REGISTER_SCRIPTABLE_COMPONENT(app, TownHall);
+    //REGISTER_SCRIPTABLE_COMPONENT(app, NPC);
+    //REGISTER_COMPONENT(app, Health);
 }
