ecf0f1fc8e20b5a3f8aa692da2f906aed863f975
Walyn <walyncurtis@gmail.com>
2025-06-07 10:00:53 -0500

    Able to create objects

--- src/ECS/ScriptableEntities/Player/PlayerBuildInput.hpp
+++ src/ECS/ScriptableEntities/Player/PlayerBuildInput.hpp
@@ -1,14 +1,17 @@
 #pragma once
-#include <SDL_keyboard.h>
 #include <string>
 
 #include <Canis/Math.hpp>
 #include <Canis/ScriptableEntity.hpp>
 #include <Canis/ECS/Components/ScriptComponent.hpp>
 #include <Canis/ECS/Components/Transform.hpp>
+#include <Canis/ECS/Components/Mesh.hpp>
+#include <Canis/ECS/Components/Color.hpp>
+#include <Canis/ECS/Components/SphereCollider.hpp>
 
 #include <Canis/Camera.hpp>
-#include <Canis/Math.hpp>
+
+using namespace Canis;
 
 class PlayerBuildInput : public Canis::ScriptableEntity
 {
@@ -19,9 +22,23 @@ class PlayerBuildInput : public Canis::ScriptableEntity
             vec2 v = GetInputManager().mouse;
             Canis::Camera c = GetCamera();
             Canis::Ray ray = Canis::RayFromScreen(c, GetWindow(), v);
-            if (ray.direction.y == 0) return; //avoid div by zero
+            if (ray.direction.y == 0) return; //avoid div by zero, ray is parallel to the ground
             float t = -(ray.origin.y/ray.direction.y); //supposed to be doing some dot products but they're all up (0,1,0) so just use y
             vec3 point = ray.origin + t * ray.direction;
-            Canis::Log(std::to_string(point.x)+","+std::to_string(point.y)+","+std::to_string(point.z));
+            point = vec3(point.x - fmod(point.x, 1), 0, point.z - fmod(point.z, 1));
+            //Canis::Log(std::to_string(point.x)+","+std::to_string(point.y)+","+std::to_string(point.z));
+            if (GetInputManager().JustLeftClicked()) {
+                Canis::Log("Trying to place building");
+                Entity newBuild = CreateEntity();
+                newBuild.AddComponent<Transform>();
+                newBuild.SetPosition(point);
+                newBuild.SetScale(vec3(.9f, 1.0f, .9f));
+                newBuild.AddComponent<SphereCollider>();
+                newBuild.AddComponent<Color>();
+                Mesh& mesh = newBuild.AddComponent<Mesh>();
+                mesh.modelHandle.id = AssetManager::LoadModel("assets/models/white_block.obj");
+                mesh.material = AssetManager::LoadMaterial("assets/materials/default.material");
+                Canis::Log(std::to_string(newBuild.GetGlobalPosition().x));
+            }
         };
 };
