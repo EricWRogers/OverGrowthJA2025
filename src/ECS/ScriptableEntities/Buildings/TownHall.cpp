ec4017b84cb65129e1e6eb7c53d996d3d208780d
Aiden Thacker <aidenthacker86@gmail.com>
2025-06-08 02:16:45 -0500

    Enemy can destroy town hall

--- src/ECS/ScriptableEntities/Buildings/TownHall.cpp
+++ src/ECS/ScriptableEntities/Buildings/TownHall.cpp
@@ -25,7 +25,7 @@ void TownHall::OnCreate()
 
 void TownHall::OnReady()
 {
-    //Call the Spawn Inital Units
+    // Call the Spawn Inital Units
     SpawnUnit();
 }
 
@@ -38,16 +38,18 @@ void TownHall::OnDestroy()
 void TownHall::OnUpdate(float _dt)
 {
     HealthComponent &health = entity.GetComponent<HealthComponent>();
-    if(health.currentHealth == 0 && m_isAlive == true)
+    if (health.currentHealth == 0 && m_isAlive)
     {
-        entity.Destroy();
+        m_isAlive == false;
+        Health::DestoryObject(entity);
     }
 
-    if(GetInputManager().JustPressedKey(SDLK_e))
+    // Can only have this uncommented if you plan to kill the Town Hall before it gets destroyed
+    /*if (GetInputManager().JustPressedKey(SDLK_e))
     {
         Canis::Log("Button Pressed");
         Health::Annihilation(entity);
-    }
+    }*/
 }
 
 void TownHall::SpawnUnit()
