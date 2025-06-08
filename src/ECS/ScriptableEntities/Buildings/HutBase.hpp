1952f0ebed36212eb7ff55ee6daae58e2f30df42
Logan <Loganhobson31@gmail.com>
2025-06-08 13:50:30 -0500

    Dang

--- src/ECS/ScriptableEntities/Buildings/HutBase.hpp
+++ src/ECS/ScriptableEntities/Buildings/HutBase.hpp
@@ -5,17 +5,25 @@
 
 #include "../AI/Job.hpp"
 
+
+class AiBrain;
+
 class HutBase : public Canis::ScriptableEntity
 {
 private:
     bool m_isAlive = true;
+    AiBrain* aiBrain = nullptr;
     
 public:
     float counter = 0;
     float max_health = 100;
     float current_health = 0;
-    float create_resource_interval = 60; //Seconds
-   
+    float job_creation_interval = 60; //Seconds
+    float create_resource_interval = 60.0f;
+
+    std::vector<glm::vec3> jobPositions;  
+    int jobPriority = 0; 
+
     Job job;
 
      HutBase() : job(this, {}, 0) {}
